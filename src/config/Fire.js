96b4e2194dc405299edf2afefec54b0d7315fd28
Sufiyan-11 <sufiyanshaikh111121998@gmail.com>
2024-03-13 17:55:49 +0530

    dashboard created

---
+++ src/config/Fire.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+require("firebase/database");
+require("firebase/auth");
+
+const config = {
+
+   apiKey: "AIzaSyBs0_abeWOl29TTa8FwvEgmnjUd5Q82wJk",
+    authDomain: "food-receipe-dashboard.firebaseapp.com",
+    projectId: "food-receipe-dashboard",
+    storageBucket: "food-receipe-dashboard.appspot.com",
+    messagingSenderId: "621956029742",
+    appId: "1:621956029742:web:6b357d9135cf97c518aebf",
+    measurementId: "G-MYY59NEWQE"
+
+};
+
+const fire = firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+export const storage = firebase.storage();
+
+export const database = firebase.database();
+
+
+export default fire;
